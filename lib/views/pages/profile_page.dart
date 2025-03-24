c739af06731deba00237ff36f42f6b91d16d24d0
Abdellah <abdallahkaba98@gmail.com>
2025-03-24 10:43:52 +0000

    .adaptive, Slider  SingleChildScrollView

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -11,58 +11,72 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController _controller = TextEditingController();
   bool? isChecked = false;
   bool isSwitch = false;
+  double sliderValue = 0.09;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextField(
-            controller: _controller,
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Text(_controller.text),
-          Checkbox(
-            tristate: true,
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          CheckboxListTile(
-            tristate: true,
-            title: Text("Click me"),
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          Switch(
-            value: isSwitch,
-            onChanged: (bool value) {
-              setState(() {
-                isSwitch = value;
-              });
-            },
-          ),
-          SwitchListTile(
-            title: Text("Switch me"),
-            value: isSwitch,
-            onChanged: (bool value) {
-              setState(() {
-                isSwitch = value;
-              });
-            },
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(_controller.text),
+            Checkbox.adaptive(
+              tristate: true,
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: Text("Click me"),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Switch.adaptive(
+              value: isSwitch,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitch = value;
+                });
+              },
+            ),
+            SwitchListTile.adaptive(
+              title: Text("Switch me"),
+              value: isSwitch,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitch = value;
+                });
+              },
+            ),
+            Slider.adaptive(
+              max: 10,
+              value: sliderValue,
+              onChanged: (double value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              },
+            ),
+            Image.asset('assets/images/ordinateur2.png'),
+            Image.asset('assets/images/ordinateur2.png'),
+          ],
+        ),
       ),
     );
   }
