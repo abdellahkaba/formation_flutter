bc3797644d0d5bdd1ef3332c77aaccb47aac19fe
Abdellah <abdallahkaba98@gmail.com>
2025-03-25 15:25:16 +0000

    Divder, Card and Text

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:demo/data/constants.dart';
 import 'package:demo/views/widgets/hero_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Column(children: [HeroWidget()]),
+      child: Column(
+        children: [
+          HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Basic Layout', style: KTextStyle.titleTealText),
+                    Text(
+                      'The Description of this',
+                      style: KTextStyle.descriptionText,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
